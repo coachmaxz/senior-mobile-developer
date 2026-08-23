f0f6aac73571179ef74af9fcb9470fab2a05a13c
Max Eakkabin Jaikeawma <eakkabin.max@outlook.com>
2026-08-23 08:18:22 +0700

    add req new hello world

--- api/functions/src/index.ts
+++ api/functions/src/index.ts
@@ -1,10 +1,9 @@
-// import * as functions from "firebase-functions";
+import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 
 admin.initializeApp();
 // const db = admin.database();
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const helloWorld = functions.https.onCall(async (data, context) => {
+  return {success: true, status: "active", data: data, context: context};
+});
